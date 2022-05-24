8a646d86992fed3ee2ceb6884e395a105188189a
Karan Banjade <karan.banjade@gmail.com>
2022-05-24 17:07:57 +0530

    changed user field to author

--- src/routers/route.js
+++ src/routers/route.js
@@ -6,15 +6,18 @@ const BlogModel = require("../models/schema");
 
 router.post('/insert', (req, res) => {
     const data = req.body;
-    const query = `insert into blogs (topic, user, description) values('${data.topic}', '${data.user}','${data.description}')`;
+    const query = `insert into blogs (topic, author, description, tags) values('${data.topic}', '${data.author}','${data.description}', '${data.tags}')`;
     sequelize.query(query, {
         type: QueryTypes.INSERT
     })
     .then((data)=>{
-        res.send(data);
+        res.send("Data Added Successfully");
         console.log(data);
     })
-    .catch(error => console.error(error));
+    .catch((error) =>{
+        res.send(error.message);
+        console.error(error);
+    } )
 })
 router.get("/get/:id", (req,res)=>{
     let id = req.params.id;
@@ -26,19 +29,25 @@ router.get("/get/:id", (req,res)=>{
         res.send(data);
         console.log(data);
     })
-    .catch(error => console.error(error));
+    .catch((error) =>{
+        res.send(error.message);
+        console.error(error);
+    } )
 })
 
 router.put("/update/:id", (req,res)=>{
     let id  = req.params.id;
     let data = req.body;
-    const query = `update blogs set topic = '${data.topic}', user = '${data.user}', description = '${data.description}' where id = ${id};`
+    const query = `update blogs set topic = '${data.topic}', author = '${data.author}', description = '${data.description}', tags = '${data.tags}' where id = ${id};`
     sequelize.query(query, {type: QueryTypes.UPDATE})
     .then((data)=>{
-        res.send(data);
+        res.send("Data Updated Successfully");
         console.log(data);
     })
-    .catch(error => console.error(error));
+    .catch((error) =>{
+        res.send(error.message);
+        console.error(error);
+    } )
 })
 
 router.delete('/delete/:id', (req,res)=>{
@@ -46,22 +55,25 @@ router.delete('/delete/:id', (req,res)=>{
     const query = `delete from blogs where id = ${id}`
     sequelize.query(query, {type: QueryTypes.DELETE})
     .then((data)=>{
-        res.send(data);
+        res.send("Data Deletd Successfully");
         console.log(data);
     })
-    .catch(error => console.error(error));
+    .catch((error) =>{
+        res.send(error.message);
+        console.error(error);
+    } )
 })
 router.get('/filter', (req,res)=>{
     const data = req.body;
-    if(data.filterType != "time"){
         const query = `select * from blogs where ${data.field} like '%${data.queryString}%'`;
         sequelize.query(query, {type: QueryTypes.SELECT})
         .then((data)=>{
             res.send(data);
             console.log(data);
         })
-        .catch(error => console.error(error));
-
-    }
+        .catch((error) =>{
+            res.send(error.message);
+            console.error(error);
+        } )
 })
 module.exports = router
